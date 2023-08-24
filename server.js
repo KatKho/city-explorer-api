bb4664d52c2c81bbc416638bd0d3fc74ba8241ed
Ekaterina Khoroshilova <ek.khorosh@gmail.com>
2023-08-23 22:54:51 -0700

    task 2

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const axios = require('axios');
 dotenv.config();
 const PORT = process.env.PORT;
 const API_KEY = process.env.WEATHER_API_KEY;
+const API_KEY_2 = process.env.MOVIE_API_KEY;
 
 const app = express();
 app.use(cors());
@@ -21,7 +22,19 @@ class Forecast {
       this.data = date;
       this.description = description;
     }
-  }
+}
+
+class Movie {
+    constructor(title, overview, average_votes, total_votes, image_url, popularity, released_on) {
+        this.title = title;
+        this.overview = overview;
+        this.average_votes = average_votes;
+        this.total_votes = total_votes;
+        this.image_url = image_url;
+        this.popularity = popularity;
+        this.released_on = released_on;
+    }
+}
 
   function handleApiError(response, error) {
     console.error('API Error:', error);
@@ -68,6 +81,74 @@ app.get('/weather', (request, response) => {
     }
 });
 
+
+app.get('/weather', (request, response) => {
+    const lat = parseFloat(request.query.lat);
+    const lon = parseFloat(request.query.lon);
+
+    if (isNaN(lat) || isNaN(lon)) {
+        response.status(400).send({ error: 'Invalid latitude, longitude, or city' });
+        return;
+    }
+
+    try {
+
+        axios.get(`http://api.weatherbit.io/v2.0/forecast/daily?key=${API_KEY}&lat=${lat}&lon=${lon}`)
+            .then(apiResponse => {
+                const weatherForecasts = apiResponse.data.data.map(forecastData => {
+                    return new Forecast(
+                        forecastData.valid_date,
+                        forecastData.weather.description,
+                    );
+                });
+
+                response.json({
+                    lat: lat,
+                    lon: lon,
+                    forecasts: weatherForecasts,
+                });
+            })
+            .catch(apiError => {
+                handleApiError(response, apiError);
+            });
+
+    } catch (error) {
+        handleApiError(response, error);
+    }
+});
+
+app.get('/movies', (request, response) => {
+    const city = request.query.city;
+
+    if (!city) {
+        response.status(400).send({ error: 'City not provided' });
+        return;
+    }
+
+    try {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY_2}&query=${city}`)
+            .then(apiResponse => {
+                const movieData = apiResponse.data.results.map(movie => {
+                    return new Movie(
+                        movie.title,
+                        movie.overview,
+                        movie.vote_average,
+                        movie.vote_count,
+                        `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                        movie.popularity,
+                        movie.release_date
+                    );
+                });
+
+                response.json(movieData);
+            })
+            .catch(apiError => {
+                handleApiError(response, apiError);
+            });
+    } catch (error) {
+        handleApiError(response, error);
+    }
+});
 app.listen(PORT, () => {
     console.log('App is listening!!');
   });
